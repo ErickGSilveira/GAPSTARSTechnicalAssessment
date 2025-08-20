5f738165bcd4b51eca3bbf0bbc51b0dbb17e836b
GLOBALREFUND\esilveira <esilveira@globalblue.com>
2025-08-20 15:53:33 +0100

    App config helpers

---
+++ Beequip.AcceptanceTests/Support/AppConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace Beequip.AcceptanceTests.Support;
+
+public class AppConfiguration
+{
+    public Playwright Playwright { get; set; }
+}
+
+public class Playwright
+{
+    public string LocalBrowser { get; set; }
+    public bool ContainerUse { get; set; }
+    public string WsEndpoint { get; set; }
+    public TimeOut TimeOut { get; set; }
+    public Login Login { get; set; }
+}
+
+public class TimeOut
+{
+    public int Default { get; set; }
+    public int Long { get; set; }
+}
+
+public class Login
+{
+    public string Basic { get; set; }
+    public string Bypass { get; set; }
+}
---
+++ Beequip.AcceptanceTests/Support/AppConfigurationHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Beequip.AcceptanceTests.Support;
+
+public static class AppConfigurationHelper
+{
+    public static AppConfiguration GetAppConfiguration()
+    {
+        string envFile;
+        var envFilePath = "../../../appsettings.json";
+
+        envFile = Environment.GetEnvironmentVariable("envFile");
+        if (envFile != null)
+        {
+            envFilePath = "../../../" + envFile + ".appsettings.json";
+        }
+        using var r = new StreamReader(envFilePath);
+        var jsonStr = r.ReadToEnd();
+        return JsonSerializer.Deserialize<AppConfiguration>(jsonStr) ?? throw new InvalidOperationException();
+    }
+
+}
